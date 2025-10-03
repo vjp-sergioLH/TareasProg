97e32fae852d3faf5aa7294a64a3bbefcc9d3eda
1DAM - Sergio Lopez Hernandez <slopezh10@informatica.iesvalledeljerteplasencia.es>
2025-10-03 16:24:52 +0200

    HolaMundo Prueba Gitdesk
    Empezamos git desktop

---
+++ Tema_2PROG/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,21 @@
+
+package holamundo;
+
+/**
+ * Mi Primer Programa
+ * @author Sergio
+ * @since 2025
+ */
+public class HolaMundo {
+
+    /**
+     * Metodo Main
+     * @since 2025
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola Mundo!!!");
+    }
+    
+}
