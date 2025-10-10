33b1e4dff93b519f641d52ca6e7ec3f9464c6e8b
1DAM - Sergio Lopez Hernandez <slopezh10@informatica.iesvalledeljerteplasencia.es>
2025-10-10 17:15:49 +0200

    Ejercicio 16

---
+++ Tema_3PROG/EJ16T3/src/ej16t3/EJ16T3.java
@@ -0,0 +1,21 @@
+package ej16t3;
+
+/**
+ *
+ * @author alumno
+ */
+public class EJ16T3 {
+
+public static void main(String[] args) {
+        int contadorImpares = 0;
+
+        System.out.println("Los números impares existentes entre el número 20 y el 160 son:");
+
+        for (int i = 21; i < 160; i += 2) {
+            System.out.print(i + " – ");
+            contadorImpares++;
+        }
+
+        System.out.println("\nLa cantidad de números impares impresos han sido: " + contadorImpares);
+    }
+}
