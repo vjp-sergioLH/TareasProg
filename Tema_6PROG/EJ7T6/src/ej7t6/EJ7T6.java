de9fc7d97b54deb9762a8baca2ae6c6cc955dd9b
1DAM - Sergio Lopez Hernandez <slopezh10@informatica.iesvalledeljerteplasencia.es>
2025-12-05 19:12:04 +0100

    EJ7T6
    Terminado

--- Tema_6PROG/EJ7T6/src/ej7t6/EJ7T6.java
+++ Tema_6PROG/EJ7T6/src/ej7t6/EJ7T6.java
@@ -10,11 +10,54 @@ package ej7t6;
  */
 public class EJ7T6 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Televisor Tele = new Televisor();
+        Radio radio = new Radio();
+        
+        Tele.encender();
+        System.out.println("");
+        System.out.println(Tele.toString());
+        System.out.println("");
+        Tele.subirVolumen();
+        Tele.subirVolumen();
+        Tele.subirVolumen();
+        System.out.println("");
+        System.out.println(Tele.toString());
+        System.out.println("");
+        Tele.getVolumen();
+        System.out.println("");
+        Tele.cambiarCanal(0);
+        System.out.println("");
+        Tele.bajarVolumen();
+        System.out.println("");
+        Tele.apagar();
+        System.out.println("");
+        System.out.println(Tele.toString());
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        
+        radio.encender();
+        System.out.println("");
+        System.out.println(radio.toString());
+        System.out.println("");
+        radio.subirVolumen();
+        System.out.println("");
+        radio.subirVolumen();
+        System.out.println("");
+        radio.subirVolumen();
+        System.out.println("");
+        radio.getEmisora();
+        System.out.println("");
+        radio.getVolumen();
+        System.out.println("");
+        radio.cambiarCanal(0);
+        System.out.println("");
+        radio.bajarVolumen();
+        System.out.println("");
+        radio.apagar();
+        System.out.println("");
+        System.out.println(radio.toString());
     }
     
 }
--- Tema_6PROG/EJ7T6/src/ej7t6/Radio.java
+++ Tema_6PROG/EJ7T6/src/ej7t6/Radio.java
@@ -1,41 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ej7t6;
 
 /**
  *
  * @author alumno
  */
-public class Televisor implements ControlRemoto{
+public class Radio implements ControlRemoto{
     
     private boolean encendido;
-    private int canal;
+    private float emisora;
     private int volumen;
     
-    public Televisor(){
+    public Radio(){
     encendido = false;
-    canal=1;
-    volumen=10;
+    emisora=8F;
+    volumen=15;
     
 }
     
-    public Televisor(boolean encendido, int canal, int volumen){
+    public Radio(boolean encendido, float emisora, int volumen){
     this.encendido = encendido;
-    this.canal=canal;
+    this.emisora=emisora;
     this.volumen=volumen;
     
     
     
 }
 
-    public int getCanal() {
-        return canal;
+    public float getEmisora() {
+        return emisora;
     }
 
-    public void setCanal(int canal) {
-        this.canal = canal;
+    public void setEmisora(float emisora) {
+        this.emisora = emisora;
     }
 
     public int getVolumen() {
@@ -54,15 +50,15 @@ public class Televisor implements ControlRemoto{
     @Override
     public void encender(){
         if (encendido == false) {
-            System.out.println(getCanal());
+            System.out.println(getEmisora());
             encendido = true;
-            System.out.println("Has encendido la pantalla. . .");
+            System.out.println("Has encendido la Radio");
         }
     }
     @Override
     public void bajarVolumen(){
         if (encendido == true) {
-            volumen -= 1;
+            volumen -= 5;
             System.out.println(getVolumen());
             System.out.println("Has bajado el volumen a: " + volumen);
         }
@@ -70,26 +66,24 @@ public class Televisor implements ControlRemoto{
     @Override
     public void subirVolumen(){
         if (encendido == true) {
-            volumen += 1;
+            volumen += 5;
             System.out.println(getVolumen());
-            System.out.println("Has Subido el volumen a: " + volumen);
+            System.out.println("Has subido el volumen a: " + volumen);
         }
     }
     @Override
-    public void cambiarCanal(float canal){
+    public void cambiarCanal(float emisora){
         if (encendido == true) {
-            this.canal = (int) canal;
-            System.out.println(getCanal());
-            System.out.println("Has cambiado el canal a: " + canal);
+            this.emisora = (int) emisora;
+            System.out.println(getEmisora());
+            System.out.println("Has cambiado la emisora a: " + emisora);
         }
     }
     
     @Override
     public String toString() {
-        return "Televisor{" + "encendido=" + encendido + ", canal=" + canal + ", volumen=" + volumen + '}';
+        return "Televisor{" + "encendido=" + encendido + ", emisora=" + emisora + ", volumen=" + volumen + '}';
     }
     
     
 }
-
-    
